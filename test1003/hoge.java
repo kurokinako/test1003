c85a04d6e40e2faa00dac3dd6915303cf0bc1d33
kurokinako <15105@hakodate.kosen-ac.jp>
2019-10-03 10:25:23 +0900

    Aがキャラ移送処理を追加

--- test1003/hoge.java
+++ test1003/hoge.java
@@ -14,6 +14,10 @@ public class hoge extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) )
+        {
+            setRotation(270);
+            move(1);
+        }
     }    
 }
